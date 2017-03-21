82e936da2660045476ddb09773a7f9124f981aa7
Christopher Baker <me@christopherbaker.net>
2017-03-21 14:37:17 -0500

    Yolo viewer.

---
+++ Week_06/01_YOLO_VIEWER/src/ofApp.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include "ofMain.h"
+
+
+class ofApp : public ofBaseApp{
+
+public:
+    void setup();
+    void update();
+    void draw();
+
+    void keyPressed(int key);
+
+    // The current image index.
+    std::size_t imageIndex = 0;
+
+    // This is the current image that we are viewing.
+    ofImage currentImage;
+
+    // A mapping from the image path, to its json data.
+    std::vector<std::pair<std::string, ofJson>> data;
+
+};
